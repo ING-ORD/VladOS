5e24561fe4fc833255b23961b33f0844fd2c0f1a
LAPTOP-V1FCBOG0\dns2018 <ignat.markov345@gmail.com>
2019-05-14 23:01:10 +0500

    VladOS пред релизная веха

--- BD_PYTHON_STUDENT.py
+++ BD_PYTHON_STUDENT.py
@@ -1,10 +1,9 @@
 def add (bd_arr = None,id = None):
-    bd_answer_add = []
     answer = []
     if(bd_arr != None):
         if (id != None ):
             for i in range(len(bd_arr)):
-                if (i == id):
+                if (i == int(id)):
                     answer.append(add_question())
                 answer.append(bd_arr[i])
             return answer
@@ -54,7 +53,9 @@ def what_change(bd_id, what):
     return answer 
 
 def table_student(bd_arr):
+
     max_len = [0,0,0,0,0,0]
+
     for ell in bd_arr:
         max_len[0] = int(len(str(len(bd_arr))))
         for i in range(1,6):   
@@ -63,50 +64,27 @@ def table_student(bd_arr):
         print(i,"|",end = " ")
     print()
 
-    # max_firs_st = int(len(str(len(bd_arr))))
-
     count = 1 
+    print("№"+" "*(max_len[0]) + "| ФАМИЛИЯ"+" "*(max_len[1]-6)+"| ИМЯ"+" "*(max_len[2]-2)+"| ОТЧЕСТВО"+" "*(max_len[3]-7)+"| ГРУППА"+" "*(max_len[4]-5)+"| ЛЕТ"+" "*(max_len[5]-2)+"|")
     for ell in bd_arr:
 
         for i in range(6):
             if (i==0):
                 if (max_len[i]%2==0 and len(ell[i])%2==0):  
-                    print(" "*((max_len[i]-len(ell[i]))//2),count," "*((max_len[i]-len(ell[i]))//2), end="|" )
-                    print(" "*((max_len[i+1]-len(ell[i]))//2),ell[i]," "*((max_len[i+1]-len(ell[i]))//2), end="|" )
-                elif (max_len[i]%2!=0 and len(ell[i])%2==0):
-                    print(" "*(((max_len[i]-len(ell[i]))//2)-1),count," "*(((max_len[i]-len(ell[i]))//2)+1), end="|" )
-                    print(" "*(((max_len[i+1]-len(ell[i]))//2)-1),ell[i]," "*(((max_len[i+1]-len(ell[i]))//2)+1), end="|" )
+                    print(count, " " * ( max_len[i] - len( str(count) ) ), end="| " )
+                    print(ell[i], " " * ( max_len[i+1] - len( ell[i] ) ), end="| " )
                 else:
-                    print(" "*(((max_len[i]-len(ell[i]))//2)-1),count," "*((max_len[i]-len(ell[i]))//2), end="|" )
-                    print(" "*(((max_len[i+1]-len(ell[i]))//2)-1),ell[i]," "*(((max_len[i+1]-len(ell[i]))//2)), end="|" )
+                    print(count, " " * ( max_len[i] - len( ell[i] ) ), end="| " )
+                    print(ell[i], " " * ( max_len[i+1] - len( ell[i] ) ), end="| " )
             if (i!=0 and i !=5):
                 if (max_len[i]%2==0 and len(ell[i])%2==0):  
-                    print(" "*((max_len[i+1]-len(ell[i]))//2),ell[i]," "*((max_len[i+1]-len(ell[i]))//2), end="|" )
+                    print(ell[i], " " * ( max_len[i+1] - len( ell[i] ) ), end="| " )
+                elif (max_len[i]%2!=0 and len(ell[i])%2==0):
+                    print(ell[i], " " * ( max_len[i+1] - len(ell[i] ) ), end="| " )
                 else:
-                    print(" "*(((max_len[i+1]-len(ell[i]))//2)-1),ell[i]," "*(((max_len[i+1]-len(ell[i]))//2)), end="|" ) 
+                    print(ell[i], " " * ( max_len[i+1] - len( ell[i] ) ), end="| " ) 
             if (i==5):
                 print()
-             
-        # print(" "*((max_len[0]-len(ell[0]) + len(ell[0])%2)//2),end = "|" )
-
-
-        # print(" "*((max_len[1]-len(ell[1]))//2), end = " ")
-        # print(ell[0], end = " ")
-        # print(" "*((max_len[0]-len(ell[0]) + len(ell[1])%2)//2), end = "|")
-        # print(" "*((max_len[1]-len(ell[1]))//2), end = " ")
-        # print(ell[1], end = " ")
-        # print(" "*((max_len[1]-len(ell[1]) + len(ell[2])%2)//2), end = "|")
-        # print(" "*((max_len[2]-len(ell[2]))//2), end = " ")
-        # print(ell[2], end = " ")
-        # print(" "*((max_len[2]-len(ell[2]) + len(ell[3])%2)//2), end = "|")
-        # print(" "*((max_len[3]-len(ell[3]))//2), end = " ")
-        # print(ell[3], end = " ")
-        # print(" "*((max_len[3]-len(ell[3]) + len(ell[4])%2)//2), end = "|")
-        # print(" "*((max_len[4]-len(ell[4]))//2), end = " ")
-        # print(ell[4], end = " ")
-        # print(" "*((max_len[4]-len(ell[4]) + len(ell[4])%2)//2), end = "|\n")
-
-
         count += 1
 
     
@@ -134,15 +112,34 @@ bd_student = [
 # add(bd,id) - Добавляет/bd - база/id - место вставки элемента,все оставльные сдвигаются, по умолчанию в конец
 # delete(bd,id) - Удаление/bd - база/id - удаляемый элемент, по умолчанию, последний
 # change(bd,what,id) - Изменение элементов/bd - база/what - что изменять,{F -Фамилия,I -Имя,O -Отчество,G -Группа,A -Возвраст}, по умолчанию перезаписывает полностью/id - изменяемый элемент, по умолчанию последний
+# table_student(bd) - Выводит содержимое на экран в таблице
 
 print("-"*25)
 print("VladOS v0.0.1 - все права защищены ©")
 print("-"*25)
 
-# for i in range(2):
-#     bd_student = add(bd_student)
-# print(change(bd_student,"A",0))
-table_student(bd_student)
+while(1):
+    req = input(":")
+    if (req =="help"):
+        print("adds - режим добавления студентов в список")
+        print("del - режим удаления студентов из списка")
+        print("change - режим коректировки элементов списка")
+        print("look - просмотр содержимого списка")
+    if (req =="adds"):
+        while(1):
+            req_adds = input("adds:")
+            if (req_adds =="help"):
+                print("add -l <позиция> - создание студента на заданной поззиции")
+                print("del - режим удаления студентов из списка")
+                print("change - режим коректировки элементов списка")
+                print("look - просмотр содержимого списка")
+            if (req_adds.split()[0] == "add" and req_adds.split()[1] =="-l"):
+                bd_student = add(bd_student,req_adds.split()[2])
+            if (req_adds == "look"):
+                table_student(bd_student)
+
+
+
 
 
 
